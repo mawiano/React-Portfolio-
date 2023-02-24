8a0cf1f9b189e7e6acbeb8b132789119f50400c3
mar <rojanomariano@gmail.com>
2023-02-24 04:23:06 -0800

    card headers for portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,6 +4,14 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 export default function Portfolio() {
   return (
-
-  );
+    <Container className="text-center mb-5">
+      <h1 className="mb-5">My projects</h1>
+      <Row>
+          {projectList.map((project) => (
+            <div key={project.id} className="col-lg-6">
+              <Card className="text-center mb-5">
+                <Card.Header>
+                  <h2>{project.title}</h2>
+                </Card.Header>
+  )
 }
