7d852db0cffd96badd6340f11ccf6c2485bb6d30
mar <rojanomariano@gmail.com>
2023-02-24 03:19:30 -0800

    importing images to use

---
+++ src/utils/projectList.js
@@ -0,0 +1,43 @@
+import nflApp from "../assets/nflApp.PNG";
+import bookApp from "../assets/book.PNG";
+import chefApp from "../assets/chefGpt.PNG"; 
+import password from "../assets/password.PNG";
+import patronApp from "../assets/patron.PNG"; 
+
+const projectList = [
+    {
+        title: "Football App",
+        description: "App that keeps live scores of all the NFL games.",
+        image: nflApp,
+        link: "https://github.com/exzilium/football-schedule",
+        id: 1,
+    },
+    {
+        title: "Book Finder",
+        description: "Allows you to search books",
+        image: bookApp,
+        link: "https://github.com/mawiano/Book-Finder",
+        id: 2,
+    },
+    {
+        title: "Chef GPT",
+        description: "Through a variety of questions, a recipe will generate.",
+        image: chefApp,
+        link: "https://github.com/csanchezwagenbach/chef-gpt",
+        id: 3,
+    },
+    {
+        title: "Password Generator",
+        description: "An app that will generate a random and secure password.",
+        image: password,
+        link: "https://github.com/mawiano/Password-Generator",
+        id: 4,
+    },
+    {
+        title: "Patron",
+        description: "Search your favorite art and save it in your own gallery.",
+        image: patronApp,
+        link: "https://github.com/Sheogorath9/Legacy-Test-Repo",
+        id: 5,
+    },
+]
