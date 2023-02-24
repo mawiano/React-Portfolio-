d5c40069e861b3f769702360a95e9a4ce1eed4a6
mar <rojanomariano@gmail.com>
2023-02-24 04:29:51 -0800

    resume download

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <div className="container">
+      <h1>Resume</h1>
+      <p>
+        <a
+          href="https://docs.google.com/document/d/1tLOmVCnCLL5pLgUOdr8TBaPIsVxtLezhj45JEaLBX1I/edit?usp=sharing"
+          target="_blank"
+          rel="noreferrer">
+            Download here
+          </a>
+      </p>
+    </div>
+  );
+}
