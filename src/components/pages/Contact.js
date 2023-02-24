ae6a13a383c57d6b0dbaa2e71ccec64aadbb20d3
mar <rojanomariano@gmail.com>
2023-02-24 03:40:59 -0800

    imports for contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
-
+import { validateEmail } from "../../utils/valid";
 
 export default function Contact() {
   return (
