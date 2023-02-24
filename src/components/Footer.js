9a899d64f3ef389c647e04def301a1082b12c87a
mar <rojanomariano@gmail.com>
2023-02-24 04:44:38 -0800

    footer email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,3 +7,22 @@ import {
   MDBIcon,
   MDBBtn,
 } from "mdb-react-ui-kit";
+
+export default function Footer() {
+    return(
+        <MDBFooter className="bg-light text-center text-white mt-5">
+            <MDBContainer className="p-4 pb-0">
+                <section className="mb-4">
+                    <MDBBtn
+                    floating
+                    className="m-1"
+                    style={{ backgroundColor "#79d1ca" }}
+                    href="mailto:rojanomariano@gmail.com"
+                    role="button">
+                         <MDBIcon fas icon="envelope" />{" "}
+                    </MDBBtn>
+                </section>
+            </MDBContainer>
+        </MDBFooter>
+    )
+}
