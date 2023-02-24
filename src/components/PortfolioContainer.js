04ef165f0350291db1ee44f5fbc14cf89cad0ac7
mar <rojanomariano@gmail.com>
2023-02-24 05:00:55 -0800

    updated page changes

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -27,11 +27,13 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div >
-      {/* We are passing the currentPage from state and the function to update it */}
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
+    <div>
+      {/* Header receives currentPage state and handlePageChange function to update page state within Navtabs */}
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Body */}
       {renderPage()}
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
