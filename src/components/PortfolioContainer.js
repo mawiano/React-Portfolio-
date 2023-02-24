828e8e6b0ee1a8f45abeb94d2832848d95de2407
mar <rojanomariano@gmail.com>
2023-02-24 01:28:04 -0800

    adding images for projects

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import NavTabs from './NavTabs';
 import Home from './pages/Home';
 import About from './pages/About';
-import Blog from './pages/Blog';
+import Projects from './pages/Blog';
 import Contact from './pages/Contact';
+import './Portfolio.css'
+import anythingPic from '../assets/coolpic.jpg'
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
@@ -17,7 +19,7 @@ export default function PortfolioContainer() {
       return <About />;
     }
     if (currentPage === 'Blog') {
-      return <Blog />;
+      return <Projects />;
     }
     return <Contact />;
   };
@@ -25,7 +27,7 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div>
+    <div >
       {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
